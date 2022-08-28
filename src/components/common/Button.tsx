d7ad7d502936cc805374954de1feb3ade071e70b
Ádám Pásztor <mrpasztoradam@gmail.com>
2022-08-28 13:50:55 +0200

    Button komponens elkészítése

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IButton {
+  children?: React.ReactNode;
+  shape?: 'circle' | 'rectangle';
+}
+
+const StyledButton = styled.button.attrs((props: IButton) => ({
+  shape: props.shape,
+}))`
+  background: #ffd124;
+  box-shadow: 1px 1px 10px rgba(255, 209, 36, 0.1);
+  font-weight: 700;
+  color: #006778;
+  height: 48px;
+  font-size: ${(props: IButton) =>
+    props.shape?.toString() === 'circle' ? '36px' : '20px'};
+  line-height: ${(props: IButton) =>
+    props.shape?.toString() === 'circle' ? '44px' : '24px'};
+  font-weight: ${(props: IButton) =>
+    props.shape?.toString() === 'circle' ? '400' : '700'};
+  width: ${(props: IButton) =>
+    props.shape?.toString() === 'circle' ? '48px' : '138px'};
+  border-radius: ${(props: IButton) =>
+    props.shape?.toString() === 'circle' ? '24px' : '8px'};
+  text-align: center;
+`;
+
+const Button = ({ shape, children }: IButton) => {
+  return <StyledButton shape={shape}>{children}</StyledButton>;
+};
+
+export default Button;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link, Outlet, useParams } from 'react-router-dom';
+import Button from '../components/common/Button';
 import Input from '../components/common/Input';
 import { getCities } from '../data/DummyData';
 import './Search.css';
+import { BiMenu } from 'react-icons/bi';
 
 let cities = getCities();
 
@@ -35,6 +37,10 @@ function Search() {
       </main>
       <footer className="footer">
         <Link to="/">Home</Link>
+        <Button shape="circle">
+          <BiMenu />
+        </Button>
+        <Button shape="rectangle">Button</Button>
       </footer>
       <Outlet />
     </React.Fragment>
