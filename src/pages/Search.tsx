3431a7cfee46dabf6a539f528cd1b1697c1c5673
Ádám Pásztor <mrpasztoradam@gmail.com>
2022-08-31 15:06:11 +0200

    Autocomplete hook, keresés eredménye megjelenik

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,11 +1,14 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import Button from '../components/common/Button';
 import './Search.css';
 import { FiMenu } from 'react-icons/fi';
 import { useDebounce } from 'usehooks-ts';
 import styled from 'styled-components';
-import { IMapsAutocomplete } from '../interfaces/IMapsAutocomplete';
+import {
+  IMapsAutocomplete,
+  IPrediction,
+} from '../interfaces/IMapsAutocomplete';
 
 const StyledInput = styled.input`
   background: #ffffff;
@@ -18,36 +21,34 @@ const StyledInput = styled.input`
   text-indent: 1rem;
 `;
 
+const useAutocomplete = (userInput: string) => {
+  const [userData, setResult] = useState<IPrediction[]>([]);
+  useEffect(() => {
+    if (userInput) {
+      fetch(
+        `https://fast-dawn-89938.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${userInput}&inputtype=textquery&key=${process.env.REACT_APP_PLACES_API}`
+      ).then((response) =>
+        response.json().then((data: IMapsAutocomplete) => {
+          console.log(data);
+          const results: IPrediction[] = data.predictions;
+          console.log(results);
+          return setResult(results);
+        })
+      );
+    }
+  }, [userInput]);
+
+  return userData;
+};
+
 function Search() {
-  const [cities, setState] = useState([]);
   const [error, setError] = useState('');
   const [value, setValue] = useState<string>('');
   const debouncedValue = useDebounce<string>(value, 1000);
+  const cities = useAutocomplete(debouncedValue);
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
-  useEffect(() => {
-    if (debouncedValue) {
-      fetch(
-        `https://fast-dawn-89938.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${debouncedValue}&inputtype=textquery&key=${process.env.REACT_APP_PLACES_API}`
-      )
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error('Sorry something went wrong');
-          }
-        })
-        .then((data) => {
-          console.log(data);
-          setState(data);
-        })
-        .catch((error) => {
-          setError(error.message);
-        });
-    }
-  }, [debouncedValue]);
-
   return (
     <React.Fragment>
       <header className="header">
@@ -56,32 +57,28 @@ function Search() {
       <main className="main">
         <div className="search-container">
           <div className="search-box">
-            <>
-              <StyledInput type="text" value={value} onChange={handleChange} />
-              {console.log(cities.length)}
-            </>
-            <div className="results-container">
-              {cities.length > 0 ? (
-                cities.map((city: IMapsAutocomplete, index) => (
-                  <div
-                    className="result"
-                    key={index}
-                    onClick={() =>
-                      console.log(
-                        `Clicked the city: ${city.predictions[0].structured_formatting.main_text}`
-                      )
-                    }
-                  >
-                    <>
-                      {console.log(cities.length.toString())}
-                      {city.predictions[index].structured_formatting.main_text}
-                    </>
-                  </div>
-                ))
-              ) : (
-                <div>Akkor jelenek meg, ha üres a találati lista tömb...</div>
-              )}
-            </div>
+            <StyledInput type="text" value={value} onChange={handleChange} />
+          </div>
+          <div className="results-container">
+            {cities.length > 0 ? (
+              cities.map((city: IPrediction, index) => (
+                <div
+                  className="result"
+                  key={index}
+                  onClick={() =>
+                    console.log(
+                      `Clicked the city: ${city.structured_formatting.main_text}`
+                    )
+                  }
+                >
+                  <Link to={`/details/${city.structured_formatting.main_text}`}>
+                    {city.structured_formatting.main_text}
+                  </Link>
+                </div>
+              ))
+            ) : (
+              <div>Akkor jelenek meg, ha üres a találati lista tömb...</div>
+            )}
           </div>
         </div>
       </main>
